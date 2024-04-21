39b69be9df1bcfdfb7547d6d48b708f28b4435dd
Nicolas Fishman <njwfish@OPENMIND7.MIT.EDU>
2024-04-21 18:00:43 -0400

    added stratified conditional flow matching

--- cfm/datamodules.py
+++ cfm/datamodules.py
@@ -38,6 +38,42 @@ class CFMDataset(torch.utils.data.Dataset):
             self.target[idx % len(self.target)],
             self.pert_mat[self.pert_ids[idx % len(self.pert_ids)]],
         )
+    
+class SCFMDataset(torch.utils.data.Dataset):
+    def __init__(self, source, target, pert_ids, pert_mat, source_strata, target_strata, probs=None, size=int(1e4)):
+        assert len(target) == len(pert_ids)
+        assert len(source) == len(source_strata)
+        assert len(target) == len(target_strata)
+        
+        self.size = size
+        self.source_strata = source_strata
+        self.target_strata = target_strata
+        self.strata = np.unique(source_strata)
+        self.num_strata = len(self.strata)
+        
+        self.source = [source[source_strata == stratum] for stratum in self.strata]
+        self.target = [target[target_strata == stratum] for stratum in self.strata]
+        self.pert_ids = [pert_ids[target_strata == stratum] for stratum in self.strata] 
+        self.pert_mat = pert_mat
+        if probs is None:
+            probs = np.ones(self.num_strata) / self.num_strata
+        self.probs = probs
+
+        
+
+    def __len__(self):
+        return self.size
+
+    def __getitem__(self, idx):
+        stratum = np.random.choice(self.num_strata, p=self.probs)
+        sidx = idx % len(self.source[stratum])
+        tidx = idx % len(self.target[stratum])
+        return (
+            self.source[stratum][sidx],
+            self.target[stratum][tidx],
+            self.pert_mat[self.pert_ids[stratum][tidx]],
+        )
+
 
 
 class torch_wrapper(torch.nn.Module):
