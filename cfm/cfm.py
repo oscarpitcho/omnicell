01bfcf26d0a79a71bd9b0b34cc6fa3d43ece483c
Nicolas Fishman <njwfish@openmind7.MIT.EDU>
2024-04-27 15:59:13 -0400

    pulled out functions and creted pipeline scipt cfm.py

---
+++ cfm/cfm.py
@@ -0,0 +1,126 @@
+from pytorch_lightning.callbacks import TQDMProgressBar
+from datamodules import FMDataset, fm_collate, CFMDataset, SCFMDataset, cfm_collate, StratifiedBatchSampler
+from torch.utils.data import RandomSampler
+from sc_etl_utils import *
+from arch import *
+from flow_utils import compute_conditional_flow
+import json
+
+import scvi
+import torch
+import numpy as np
+import pytorch_lightning as pl
+import os
+
+import scanpy as sc
+
+from torchcfm.conditional_flow_matching import *
+import scanpy as sc
+import hashlib
+
+import argparse
+parser = argparse.ArgumentParser(description='Fit fm.')
+parser.add_argument('-b','--batch_size',help='Batch size', type=int, default=32)
+parser.add_argument('-m','--max_epochs',help='Max epochs', type=int, default=100)
+parser.add_argument('--dataset', help='Dataset adata file', type=str, required=True)
+parser.add_argument('--control_pert', help='Name of control in perturbation column', type=str, required=True)
+parser.add_argument('--holdout_cell', help='Name of hold out cell type in cell type column', type=str, required=True)
+parser.add_argument('--holdout_pert', help='Name of hold out perturbation in perturbation column', type=str, required=True)
+parser.add_argument('--cell_col', help='Name of cell type column', type=str, default="cell_type")
+parser.add_argument('--pert_col', help='Name of perturbation column', type=str, default="pert_type")
+parser.add_argument('-s', help='Stratify sample', action='store_true')
+parser.add_argument('--exclude_ct', help='Exclude cell type from conditioning features', action='store_true')
+parser.add_argument('-e', '--embedding', help='Name of embedding', type=str, default="standard")
+parser.add_argument('-a', '--arch', help='Name of arch', type=str, default="cmlp")
+
+args = parser.parse_args()
+print(args)
+
+batch_size = args.batch_size
+max_epochs = args.max_epochs
+cell_col, pert_col = args.cell_col, args.pert_col
+control_pert, holdout_cell, holdout_pert = args.control_pert, args.holdout_cell, args.holdout_pert
+embedding = args.embedding
+strat = args.s
+dataset = args.dataset # 'Seurat_object_TGFB_Perturb_seq.h5ad'
+arch = args.arch
+cell_type_features = not args.exclude_ct
+
+hash_dir = hashlib.sha256(json.dumps(args.__dict__, sort_keys=True).encode()).hexdigest()
+save_path = f"{dataset}/{hash_dir}"
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+
+with open(f"{save_path}/config.json", 'w') as f:
+    json.dump(args.__dict__, f, indent=2)
+
+print("Loading dataset")
+# load some data
+adata = sc.read_h5ad(f'/orcd/archive/abugoot/001/Projects/dlesman/datasets/{dataset}.h5ad')
+
+print("Splitting dataset")
+control_idx, pert_idx, eval_idx, eval_cell_idx, eval_pert_idx = get_train_eval_idxs(
+    adata, control_pert, holdout_cell, holdout_pert, cell_col=cell_col, pert_col=pert_col
+)
+
+pert_ids, pert_mat, cell_types = get_identity_features(
+    adata, cell_col=cell_col, pert_col=pert_col, cell_type_features=cell_type_features
+)
+
+adata.obsm["standard"] = adata.X
+X = adata.obsm[embedding]
+X = X.toarray()
+
+control_train, pert_train, pert_ids_train, control_cell_types, pert_cell_types, control_eval, pert_eval, pert_ids_eval = get_train_eval(
+    X, pert_ids, cell_types, control_idx, pert_idx, eval_idx, eval_cell_idx, eval_pert_idx
+)
+
+print("Creating dataloader")
+if strat:
+    # set up data processing for cfm
+    dset = SCFMDataset(
+        control_train, pert_train, 
+        pert_ids_train, pert_mat, 
+        control_cell_types, pert_cell_types,
+        batch_size=batch_size, size=X.shape[0]
+    )
+    dl = torch.utils.data.DataLoader(
+        dset, collate_fn=cfm_collate, 
+        batch_sampler=StratifiedBatchSampler(
+            RandomSampler(dset), batch_size=batch_size, drop_last=True, 
+            probs=dset.probs, num_strata=dset.num_strata
+        ), num_workers=80
+    )
+else:
+    raise NotImplemented
+
+
+print("Training model")
+# Train the model
+trainer = pl.Trainer(
+    gpus=1,  # Specify the number of GPUs to use
+    max_epochs=max_epochs,  # Specify the maximum number of training epochs
+    default_root_dir=save_path,
+    callbacks=[TQDMProgressBar(refresh_rate=10)]
+)
+
+if arch.lower() == 'cmlp':
+    model = CMLP(feat_dim=X.shape[1], cond_dim=pert_mat.shape[1], time_varying=True)
+elif arch.lower() == 'cmha':
+    model = CMLP(feat_dim=X.shape[1], cond_dim=pert_mat.shape[1], time_varying=True)
+else:
+    raise NotImplemented
+    
+trainer.fit(model, dl)
+
+print("Computing predictions")
+
+# Save the predicted perturbation
+torch.cuda.empty_cache()
+traj = compute_conditional_flow(model, control_eval, pert_ids_eval, pert_mat)
+    
+print("Saving predictions")
+np.savez(
+    f"{save_path}/pred.npz", 
+    pred_pert=traj[-1, :, :], true_pert=pert_eval, control=control_eval
+)
--- cfm/datamodules.py
+++ cfm/datamodules.py
@@ -3,7 +3,77 @@ from torchcfm.conditional_flow_matching import (
     ExactOptimalTransportConditionalFlowMatcher,
 )
 
+from torch.utils.data import BatchSampler, SequentialSampler, Sampler
+from typing import Iterator, Iterable, Optional, Sequence, List, TypeVar, Generic, Sized, Union
+
 import numpy as np
+
+class StratifiedBatchSampler(Sampler[List[int]]):
+    r"""Wraps another sampler to yield a mini-batch of indices.
+
+    Args:
+        sampler (Sampler or Iterable): Base sampler. Can be any iterable object
+        batch_size (int): Size of mini-batch.
+        drop_last (bool): If ``True``, the sampler will drop the last batch if
+            its size would be less than ``batch_size``
+
+    Example:
+        >>> list(BatchSampler(SequentialSampler(range(10)), batch_size=3, drop_last=False))
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8], [9]]
+        >>> list(BatchSampler(SequentialSampler(range(10)), batch_size=3, drop_last=True))
+        [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
+    """
+
+    def __init__(
+        self, sampler: Union[Sampler[int], Iterable[int]], batch_size: int, drop_last: bool, probs, num_strata
+    ) -> None:
+        # Since collections.abc.Iterable does not check for `__getitem__`, which
+        # is one way for an object to be an iterable, we don't do an `isinstance`
+        # check here.
+        if not isinstance(batch_size, int) or isinstance(batch_size, bool) or \
+                batch_size <= 0:
+            raise ValueError(f"batch_size should be a positive integer value, but got batch_size={batch_size}")
+        if not isinstance(drop_last, bool):
+            raise ValueError(f"drop_last should be a boolean value, but got drop_last={drop_last}")
+        self.sampler = sampler
+        self.batch_size = batch_size
+        self.drop_last = drop_last
+        self.probs = probs
+        self.num_strata = num_strata
+
+    def __iter__(self) -> Iterator[List[int]]:
+        # Implemented based on the benchmarking in https://github.com/pytorch/pytorch/pull/76951
+        stratum = np.repeat(np.random.choice(self.num_strata, p=self.probs), self.batch_size)
+        if self.drop_last:
+            sampler_iter = iter(self.sampler)
+            while True:
+                try:
+                    batch = [next(sampler_iter) for _ in range(self.batch_size)]
+                    yield zip(stratum, batch)
+                except StopIteration:
+                    break
+        else:
+            batch = [0] * self.batch_size
+            idx_in_batch = 0
+            for idx in self.sampler:
+                batch[idx_in_batch] = idx
+                idx_in_batch += 1
+                if idx_in_batch == self.batch_size:
+                    yield zip(stratum, batch)
+                    idx_in_batch = 0
+                    batch = [0] * self.batch_size
+            if idx_in_batch > 0:
+                yield zip(stratum, batch[:idx_in_batch])
+
+    def __len__(self) -> int:
+        # Can only be called if self.sampler has __len__ implemented
+        # We cannot enforce this condition, so we turn off typechecking for the
+        # implementation below.
+        # Somewhat related: see NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
+        if self.drop_last:
+            return len(self.sampler) // self.batch_size  # type: ignore[arg-type]
+        else:
+            return (len(self.sampler) + self.batch_size - 1) // self.batch_size  # type: ignore[arg-type]
 
 
 class FMDataset(torch.utils.data.Dataset):
@@ -44,12 +114,12 @@ class SCFMDataset(torch.utils.data.Dataset):
         self, source, target, pert_ids, pert_mat, source_strata, target_strata, 
         probs=None, size=int(1e4), batch_size=32
     ):
-        assert len(target) == len(pert_ids)
-        assert len(source) == len(source_strata)
-        assert len(target) == len(target_strata)
-        
         source, target = np.array(source), np.array(target)
         pert_ids, pert_mat = np.array(pert_ids), np.array(pert_mat)
+        
+        assert target.shape[0] == pert_ids.shape[0]
+        assert source.shape[0] == source_strata.shape[0]
+        assert target.shape[0] == target_strata.shape[0]
         
         self.size = size
         self.source_strata = source_strata
@@ -69,25 +139,19 @@ class SCFMDataset(torch.utils.data.Dataset):
             probs /= probs.sum()
             print(probs)
         self.probs = probs
-        self.stratum = 0
-        self.batch_pos = 0
-        self.batch_size = batch_size
-
         
 
     def __len__(self):
         return self.size
 
-    def __getitem__(self, idx):
-        if self.batch_pos % self.batch_size == 0:
-            self.stratum = np.random.choice(self.num_strata, p=self.probs)
-        self.batch_pos += 1
-        sidx = idx % len(self.source[self.stratum])
-        tidx = idx % len(self.target[self.stratum])
+    def __getitem__(self, strata_idx):
+        stratum, idx = strata_idx
+        sidx = idx % len(self.source[stratum])
+        tidx = idx % len(self.target[stratum])
         return (
-            self.source[self.stratum][sidx],
-            self.target[self.stratum][tidx],
-            self.pert_mat[self.pert_ids[self.stratum][tidx]],
+            self.source[stratum][sidx],
+            self.target[stratum][tidx],
+            self.pert_mat[self.pert_ids[stratum][tidx]],
         )
 
 
