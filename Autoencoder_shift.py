811dc9ed54311dec3bc8d1113441b6ffa7bd4192
jason-lequyer <163599822+jason-lequyer@users.noreply.github.com>
2024-06-24 17:51:04 -0400

    Add files via upload

--- Autoencoder_shift.py
+++ Autoencoder_shift.py
@@ -1,22 +1,154 @@
-
+import os
+os.environ[ 'NUMBA_CACHE_DIR' ] = '/scratch/l/ltri2014/jasonleq/'
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
-import os
 import sys
 import numpy as np
-from pathlib import Path
-import time
 import scanpy as sc
 import anndata as ad
 import scipy
 from scipy.sparse import issparse
 import anndata
-import scanpy as sc
-import numpy as np
 import pandas as pd  
-from matplotlib import pyplot as plt
+import pickle
+
+
+
+MAX_P_VAL = 0.05
+P_VAL_ITERS = 10000
+REPLICATES = 10
+
+def get_DEG_with_direction(gene, score):
+    if score > 0:
+        return(f'{gene}+')
+    else:
+        return(f'{gene}-')
+        
+def to_dense(X):
+    if issparse(X):
+        return X.toarray()
+    else:
+        return np.asarray(X)
+
+def get_DEGs(control_adata, target_adata):
+    temp_concat = anndata.concat([control_adata, target_adata], label = 'batch')
+    sc.tl.rank_genes_groups(temp_concat, 'batch', method='wilcoxon', 
+                                groups = ['1'], ref = '0', rankby_abs = True)
+
+    rankings = temp_concat.uns['rank_genes_groups']
+    result_df = pd.DataFrame({'scores': rankings['scores']['1'],
+                     'pvals_adj': rankings['pvals_adj']['1']},
+                    index = rankings['names']['1'])
+    return result_df
+
+def get_eval(true_adata, pred_adata, DEGs, DEG_vals, pval_threshold):
+        
+    results_dict =  {}
+    
+    true_mean = to_dense(true_adata.X).mean(axis = 0)
+    true_var = to_dense(true_adata.X).var(axis = 0)
+    
+    pred_mean = to_dense(pred_adata.X).mean(axis = 0)
+    pred_var = to_dense(pred_adata.X).var(axis = 0)
+    
+    true_corr_mtx = np.corrcoef(to_dense(true_adata.X), rowvar=False).flatten()
+    true_cov_mtx = np.cov(to_dense(true_adata.X), rowvar=False).flatten()
+        
+    pred_corr_mtx = np.corrcoef(to_dense(pred_adata.X), rowvar=False).flatten()
+    pred_cov_mtx = np.cov(to_dense(pred_adata.X), rowvar=False).flatten()
+
+    results_dict['all_genes_mean_R2'] = scipy.stats.pearsonr(true_mean, pred_mean)[0]**2
+    results_dict['all_genes_var_R2'] = scipy.stats.pearsonr(true_var, pred_var)[0]**2
+    results_dict['all_genes_mean_MSE'] = (np.square(true_mean - pred_mean)).mean(axis=0)
+    results_dict['all_genes_var_MSE'] = (np.square(true_var - pred_var)).mean(axis=0)
+   
+    corr_nas = np.logical_or(np.isnan(true_corr_mtx), np.isnan(pred_corr_mtx))
+    cov_nas = np.logical_or(np.isnan(true_cov_mtx), np.isnan(pred_cov_mtx))
+        
+    results_dict['all_genes_corr_mtx_R2'] = scipy.stats.pearsonr(true_corr_mtx[~corr_nas], pred_corr_mtx[~corr_nas])[0]**2
+    results_dict['all_genes_cov_mtx_R2'] = scipy.stats.pearsonr(true_cov_mtx[~cov_nas], pred_cov_mtx[~cov_nas])[0]**2
+    results_dict['all_genes_corr_mtx_MSE'] = (np.square(true_corr_mtx[~corr_nas] - pred_corr_mtx[~corr_nas])).mean(axis=0)
+    results_dict['all_genes_cov_mtx_MSE'] = (np.square(true_cov_mtx[~cov_nas] - pred_cov_mtx[~cov_nas])).mean(axis=0)
+
+    significant_DEGs = DEGs[DEGs['pvals_adj'] < pval_threshold]
+    num_DEGs = len(significant_DEGs)
+    DEG_vals.insert(0, num_DEGs)
+    
+    for val in DEG_vals:
+        if ((val > num_DEGs) or (val == 0)):
+            results_dict[f'Top_{val}_DEGs_mean_R2'] = None
+            results_dict[f'Top_{val}_DEGs_var_R2'] = None
+            results_dict[f'Top_{val}_DEGs_mean_MSE'] = None
+            results_dict[f'Top_{val}_DEGs_var_MSE'] = None
+                        
+            results_dict[f'Top_{val}_DEGs_corr_mtx_R2'] = None
+            results_dict[f'Top_{val}_DEGs_cov_mtx_R2'] = None
+            results_dict[f'Top_{val}_DEGs_corr_mtx_MSE'] = None
+            results_dict[f'Top_{val}_DEGs_cov_mtx_MSE'] = None
+        
+        else:
+            top_DEGs = significant_DEGs[0:val].index
+        
+            true_mean = to_dense(true_adata[:,top_DEGs].X).mean(axis = 0)
+            true_var = to_dense(true_adata[:,top_DEGs].X).var(axis = 0)
+            true_corr_mtx = np.corrcoef(to_dense(true_adata[:,top_DEGs].X), rowvar=False).flatten()
+            true_cov_mtx = np.cov(to_dense(true_adata[:,top_DEGs].X), rowvar=False).flatten()
+
+            pred_mean = to_dense(pred_adata[:,top_DEGs].X).mean(axis = 0)
+            pred_var = to_dense(pred_adata[:,top_DEGs].X).var(axis = 0)
+            pred_corr_mtx = np.corrcoef(to_dense(pred_adata[:,top_DEGs].X), rowvar=False).flatten()
+            pred_cov_mtx = np.cov(to_dense(pred_adata[:,top_DEGs].X), rowvar=False).flatten()
+
+            results_dict[f'Top_{val}_DEGs_mean_R2'] = scipy.stats.pearsonr(true_mean, pred_mean)[0]**2
+            results_dict[f'Top_{val}_DEGs_var_R2'] = scipy.stats.pearsonr(true_var, pred_var)[0]**2
+            results_dict[f'Top_{val}_DEGs_mean_MSE'] = (np.square(true_mean - pred_mean)).mean(axis=0)
+            results_dict[f'Top_{val}_DEGs_var_MSE'] = (np.square(true_var - pred_var)).mean(axis=0)
+            
+            corr_nas = np.logical_or(np.isnan(true_corr_mtx), np.isnan(pred_corr_mtx))
+            cov_nas = np.logical_or(np.isnan(true_cov_mtx), np.isnan(pred_cov_mtx))
+            
+            results_dict[f'Top_{val}_DEGs_corr_mtx_R2'] = scipy.stats.pearsonr(true_corr_mtx[~corr_nas], pred_corr_mtx[~corr_nas])[0]**2
+            results_dict[f'Top_{val}_DEGs_cov_mtx_R2'] = scipy.stats.pearsonr(true_cov_mtx[~cov_nas], pred_cov_mtx[~cov_nas])[0]**2
+            results_dict[f'Top_{val}_DEGs_corr_mtx_MSE'] = (np.square(true_corr_mtx[~corr_nas] - pred_corr_mtx[~corr_nas])).mean(axis=0)
+            results_dict[f'Top_{val}_DEGs_cov_mtx_MSE'] = (np.square(true_cov_mtx[~cov_nas] - pred_cov_mtx[~cov_nas])).mean(axis=0)
+
+    return results_dict
+
+def get_DEG_Coverage_Recall(true_DEGs, pred_DEGs, p_cutoff):
+    sig_true_DEGs = true_DEGs[true_DEGs['pvals_adj'] < p_cutoff]
+    true_DEGs_with_direction = [get_DEG_with_direction(gene,score) for gene, score in zip(sig_true_DEGs.index, sig_true_DEGs['scores'])]
+    sig_pred_DEGs = pred_DEGs[pred_DEGs['pvals_adj'] < p_cutoff]
+    pred_DEGs_with_direction = [get_DEG_with_direction(gene,score) for gene, score in zip(sig_pred_DEGs.index, sig_pred_DEGs['scores'])]
+    num_true_DEGs = len(true_DEGs_with_direction)
+    num_pred_DEGs = len(pred_DEGs_with_direction)
+    num_overlapping_DEGs = len(set(true_DEGs_with_direction).intersection(set(pred_DEGs_with_direction)))
+    if num_true_DEGs > 0: 
+        COVERAGE = num_overlapping_DEGs/num_true_DEGs
+    else:
+        COVERAGE = None
+    if num_pred_DEGs > 0:
+        RECALL = num_overlapping_DEGs/num_pred_DEGs
+    else:
+        RECALL = None
+    return COVERAGE, RECALL
+
+def get_DEGs_overlaps(true_DEGs, pred_DEGs, DEG_vals, pval_threshold):
+    true_DEGs_for_comparison = [get_DEG_with_direction(gene,score) for gene, score in zip(true_DEGs.index, true_DEGs['scores'])]   
+    pred_DEGs_for_comparison = [get_DEG_with_direction(gene,score) for gene, score in zip(pred_DEGs.index, pred_DEGs['scores'])]
+
+    significant_DEGs = true_DEGs[true_DEGs['pvals_adj'] < pval_threshold]
+    num_DEGs = len(significant_DEGs)
+    DEG_vals.insert(0, num_DEGs)
+    
+    results = {}
+    for val in DEG_vals:
+        if val > num_DEGs:
+            results[f'Overlap_in_top_{val}_DEGs'] = None
+        else:
+            results[f'Overlap_in_top_{val}_DEGs'] = len(set(true_DEGs_for_comparison[0:val]).intersection(set(pred_DEGs_for_comparison[0:val])))
+
+    return results
 
 
 
@@ -24,17 +156,23 @@ from matplotlib import pyplot as plt
 #Assumption: at least 3 cell types
 learning_rate = 0.00005
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-inp = sc.read("input/nault.h5ad")
+datasetso = sys.argv[1]
+#datasetso = 'kang'
+
+inp = sc.read(f'input/{datasetso}.h5ad')
+sc.pp.normalize_total(inp,target_sum=10000)
 numvars = len(inp.var_names)
 
 ctkey = 'cell_type'
 ptkey = 'condition_key'
 ptctrlkey = 'control'
 
-holdoutcell = 'Macrophage'
+holdoutcell = sys.argv[2]
+#holdoutcell = 'NK'
 
 holdoutidx = (inp.obs[ctkey]==holdoutcell)
 train = inp[~holdoutidx].copy()
+
 valid = inp[holdoutidx].copy()
 
 celltypes= np.unique(train.obs[ctkey])
@@ -88,7 +226,7 @@ goodo=True
 counter=0
 batsize = 16
 valbatsize = 64
-latent_dim = inp.shape[1]//2
+latent_dim = 2500
 
 class Lin(nn.Module):
 
@@ -153,6 +291,8 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(net.parameters(), lr=learning_rate)
 running_loss = 0
 running_loss_recon = 0
+running_loss_delta = 0
+running_loss_delta_counter = 0
 
 recondata = [valdata[0]]
 reconcts = len(recondata[0])
@@ -172,13 +312,24 @@ for pt2 in noctrlptslst:
 
 while goodo:
     counter += 1
-    if counter % 10000 == 0:
+    if counter % 2000000 == 0:
+        goodo = False
+    if counter % 250000 == 0:
         with torch.no_grad():
             print(counter)
             print('latent loss:')
             print(running_loss)
             print('recon loss:')
             print(running_loss_recon)
+            print('abs diff with delta')
+            print(running_loss_delta/running_loss_delta_counter)
+            diffdelta_filename = f'diffdelta_autoencoder_{counter}_{holdoutcell}.pkl'
+            
+            with open(f'{diffdelta_filename}', 'wb') as f:
+                pickle.dump(np.array([running_loss_delta/running_loss_delta_counter]), f)
+                
+            running_loss_delta = 0
+            running_loss_delta_counter = 0
             running_loss = 0
             running_loss_recon = 0
             pt1 = ptctrlidx
@@ -223,17 +374,15 @@ while goodo:
             pred_dan = recondata[pt2][ct2].cpu().detach().numpy()
             
             pred_pert = anndata.AnnData(X=pred_dan)
-            sc.pp.normalize_total(pred_pert)
+            sc.pp.normalize_total(pred_pert,target_sum=10000)
             pred_pert.obs['condition_key'] = 'predicted'
             
             
             
             true_pert = anndata.AnnData(X=pert_dan)
-            sc.pp.normalize_total(true_pert)
             true_pert.obs['condition_key'] = 'perturbed'
             
             control = anndata.AnnData(X=cont_dan)
-            sc.pp.normalize_total(control)
             control.obs['condition_key'] = 'control'
             
             control.obs_names = control.obs_names+'-1'
@@ -247,17 +396,15 @@ while goodo:
             
             sc.pp.neighbors(allcon, n_neighbors=15)
             sc.tl.umap(allcon, min_dist=0.1, random_state=42)
-            with plt.rc_context({'axes.spines.right': False, 'axes.spines.top': False}):
-                # Generate the PCA plot
-                ax = sc.pl.umap(allcon, color=["condition_key"], groups=["control", "perturbed","predicted"], show=False)
-                #ax = sc.pl.umap(curdata, color=["condition_key"], groups=["perturbed_decoded","perturbed"], show=False)
-                
-                
-        
-    
-                
-                # Show the plot
-                plt.show()
+            
+            
+            umap_filename = f'UMAP_autoencoder_{counter}_{holdoutcell}.pkl'
+            
+            with open(f'{umap_filename}', 'wb') as f:
+                pickle.dump(allcon.obsm['X_umap'], f)
+
+            
+
             
             
             
@@ -278,6 +425,47 @@ while goodo:
             
 
             print('myDEGs: '+str(np.intersect1d(degsGT,degsP).shape[0]))
+            
+            pred_file_name = f'autoencoder__autoencoder_{counter}_{holdoutcell}.npz' 
+            r2_mse_filename = f'r2_and_mse_autoencoder_{counter}_{holdoutcell}.pkl'
+            c_r_filename = f'c_r_results_autoencoder_{counter}_{holdoutcell}.pkl'
+            DEGs_overlap_filename = f'DEGs_overlaps_autoencoder_{counter}_{holdoutcell}.pkl'
+            
+            
+            
+            print(pred_file_name)
+            
+             
+            
+            
+            #control_subsamples_dict = {} 
+            #pred_pert_subsamples_dict = {} 
+            #true_pert_subsamples_dict = {}
+            #subsample_size = int(len(true_pert.obs.index)*0.95)
+            #for i in range(REPLICATES):
+            #    control_subsamples_dict[i] = control[np.random.choice(control.obs.index, size=subsample_size, replace=False)]
+            #    pred_pert_subsamples_dict[i] = pred_pert[np.random.choice(pred_pert.obs.index, size=subsample_size, replace=False)]
+            #    true_pert_subsamples_dict[i] = true_pert[np.random.choice(true_pert.obs.index, size=subsample_size, replace=False)]
+        
+            true_DEGs_df = get_DEGs(control, true_pert)
+            pred_DEGs_df = get_DEGs(control, pred_pert)
+        
+            r2_and_mse = get_eval(true_pert, pred_pert, true_DEGs_df, [100,50,20], 0.05)
+            c_r_results = {p: get_DEG_Coverage_Recall(true_DEGs_df, pred_DEGs_df, p) for p in [x/P_VAL_ITERS for x in range(1,int(P_VAL_ITERS*MAX_P_VAL))]}
+            DEGs_overlaps = get_DEGs_overlaps(true_DEGs_df, pred_DEGs_df, [100,50,20], 0.05)
+        
+            try:
+                with open(f'{r2_mse_filename}', 'wb') as f:
+                    pickle.dump(r2_and_mse, f)
+        
+                with open(f'{c_r_filename}', 'wb') as f:
+                    pickle.dump(c_r_results, f)
+        
+                with open(f'{DEGs_overlap_filename}', 'wb') as f:
+                    pickle.dump(DEGs_overlaps, f)
+
+            except Exception as error:
+                print('An error occured:', error)
 
 
 
@@ -331,17 +519,20 @@ while goodo:
     
 
     lossrecon = criterion(c1p1_decod,c1p1_lst) + criterion(c1p2_decod,c1p2_lst[:,rand_idx,:])
-    losslatent = criterion((c1p2_lat[:,closest_idx:closest_idx+1,:]-c1p1_lat),dummy) 
+    losslatent = criterion((c1p2_lat[:,closest_idx:closest_idx+1,:]-c1p1_lat),dummy)
+    with torch.no_grad():
+        losslatent_delta = torch.mean(torch.abs(c1p2_lat[:,closest_idx:closest_idx+1,:]-c1p1_lat-1))
     
     loss = lossrecon+losslatent
 
     
     running_loss+=losslatent.item()
     running_loss_recon+=lossrecon.item()
+    running_loss_delta+=losslatent_delta.item()
+    running_loss_delta_counter+=1
     loss.backward()
     optimizer.step()
 
 
 
     
-
