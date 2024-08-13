78138f4c9dedc39e9883966d08c69a15b540296c
jason-lequyer <163599822+jason-lequyer@users.noreply.github.com>
2024-08-13 11:14:03 -0400

    Update GetNN.py

--- GetNN.py
+++ GetNN.py
@@ -18,6 +18,9 @@ inp = sc.read(f'input/{datasetso}.h5ad')
 inp = inp[inp.obs['cell_type']=='A549']
 inp.var_names = inp.var['gene']
 
+sc.pp.normalize_total(inp,target_sum=10000)
+
+sc.pp.log1p(inp)
 
 
 # Extract the unique genes
@@ -91,8 +94,6 @@ for sr in significant_reducers:
         bestnnscore = nnscore
 
 
-print('Nearest neighbor perturbation is: '+nnbr)
-
-sys.exit()
+print('Nearest Neighbor Cell is: '+nnbr)
     
     
