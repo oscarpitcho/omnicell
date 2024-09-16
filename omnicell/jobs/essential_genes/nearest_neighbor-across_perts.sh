8445b9142348761396df7ea72334727d192f2d9d
Oscar Pitcho <opitcho@ethz.ch>
2024-09-16 15:59:17 -0400

    essential genes jobs

---
+++ omnicell/jobs/essential_genes/nearest_neighbor-across_perts.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -t 48:00:00          # walltime = 48 hours
+#SBATCH -n 4                 # 4 CPU cores
+#SBATCH --mem=256G           # memory per node
+hostname                     # Print the hostname of the compute node
+
+# Define arrays for holdouts and targets
+
+
+
+source ~/.bashrc
+mamba activate sandbox
+
+# Run the training script
+python train.py --task_config configs/tasks/essential_genes/raw/essential_genes_across_genes_basic_ood.yaml \
+    --model_config configs/models/nearest-neighbor/nearest-neighbor_substitute.yaml -l DEBUG
+
+echo "Generating evaluations"
+
+python generate_evaluations.py --model_name nearest-neighbor_substitute --task_name essential_genes_across_genes_basic_ood
+
+echo "End of bash script"
