40c1dd5bcbb09cc39a40d0f270cadb0577487554
njwfish <njwfish@gmail.com>
2024-09-27 18:08:28 -0400

    flow updates

--- omnicell/omnicell/models/llm/llm_predictor.py
+++ omnicell/omnicell/models/llm/llm_predictor.py
@@ -4,6 +4,13 @@ import yaml
 import numpy as np
 import math 
 from omnicell.models.llm import MAE
+from omnicell.models.sc_etl_utils import *
+
+from torchcfm.conditional_flow_matching import *
+import scanpy as sc
+from datamodules import  SCFMDataset, cfm_collate, StratifiedBatchSampler, ot_collate
+
+from omnicell.constants import CELL_KEY, CONTROL_PERT, PERT_KEY
 from OTmap import *
 
 class LLMPredictor():
@@ -17,17 +24,24 @@ class LLMPredictor():
         self.device = device
 
         base_learning_rate = 5e-5
-        
+
         self.model = model = MAE(
             input_size, 
             self.model_config['emb_dim'], 
             self.model_config['decoder_layer'],
-             self.model_config['encoder_layer'], 
-             self.model_config['encoder_head'], 
-             self.model_config['decoder_head'],
-             self.model_config
-             ff_dim=128,
-        true_sparsity=False, expr_activation="sigmoid")
+            self.model_config['encoder_layer'], 
+            self.model_config['encoder_head'], 
+            self.model_config['decoder_head'],
+            self.model_config['ff_dim'],
+            true_sparsity=False, 
+            expr_activation="sigmoid"
+        )
+
+        base_learning_rate = 5e-5
+        weight_decay=0.0
+        total_epoch = 500
+        warmup_epoch = 10
+        self.minibatch_size = 128 
 
         self.optim = torch.optim.AdamW(model.parameters(), lr=base_learning_rate, betas=(0.9, 0.999), weight_decay=weight_decay)
         self.lr_func = lambda epoch: min((epoch + 1) / (warmup_epoch + 1e-5), 0.5 * (math.cos(epoch / total_epoch * math.pi) + 1))
@@ -43,27 +57,48 @@ class LLMPredictor():
 
 
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        cell_col = 'cell_type'
+        pert_col = 'pert_type'
+        control_pert, holdout_cells, holdout_perts = CONTROL_PERT, [], []
 
-        inp = adata 
-        inp.var_names = inp.var['gene']
+        gene_map = {k: i for i, k in enumerate(adata.var.index)}
+        gene_map = gene_map | {'NT': max(gene_map.values()) + 1}
+        adata.obs['pert_type'] = adata.obs.gene.map(gene_map)
+        pert_ids = np.array(adata.obs['pert_type'])
+        pert_mat = np.arange(pert_ids.max() + 1)[:, None]
 
+        control_idx, pert_idx, eval_idx, eval_cell_idx, eval_pert_idx = get_train_eval_idxs(
+            adata, control_pert, holdout_cells, holdout_perts, cell_col=CELL_KEY, pert_col=PERT_KEY
+        )
 
-        # Convert sparse matrix to dense
-        inp.X = np.array(inp.X.todense())
+        _, _, cell_types = get_identity_features(
+            adata, cell_col=cell_col, pert_col=pert_col, cell_type_features=False
+        )
 
-        # Convert the relevant obs and var columns to numpy arrays
-        inp.obs['nCount_RNA'] = inp.obs['nCount_RNA'].values
-        inp.obs['gene'] = inp.obs['gene'].values
-        inp.obs['cell_type'] = inp.obs['cell_type'].values
-        inp.var['gene'] = inp.var['gene'].values
+        adata.obsm["standard"] = adata.X
+        X = adata.obsm["standard"]
+        X = X.toarray()
+        X = X / X.sum(axis=1)[:, None]
 
-        ot_mappings, valid_perturbed_indices = compute_ot_mapping(inp, cost_threshold=0.01)
-        
-        inp.X = inp.X.toarray()
-        inp.X = inp.X / inp.X.sum(axis=1)[:, None]
-        
-        dataset = OTMappingDataset(inp, ot_mappings, valid_perturbed_indices)
-        dl = DataLoader(dataset, batch_size=32, shuffle=True)
+        control_train, pert_train, pert_ids_train, control_cell_types, pert_cell_types, control_eval, pert_eval, pert_ids_eval = get_train_eval(
+            X, pert_ids, cell_types, control_idx, pert_idx, eval_idx, eval_cell_idx, eval_pert_idx
+        )
+
+        batch_size = 512
+        dset = SCFMDataset(
+            control_train, pert_train, 
+            pert_ids_train, pert_mat, 
+            control_cell_types, pert_cell_types,
+            batch_size=batch_size, size=X.shape[0]
+        )
+        ns = np.array([[t.shape[0] for t in ts] for ts in dset.target])
+        dl = torch.utils.data.DataLoader(
+            dset, collate_fn=ot_collate, 
+            batch_sampler=StratifiedBatchSampler(
+                ns=ns, batch_size=512
+            )
+        )
+
         ### CODE FROM AE TRAINING LOOP ###
 
         step_count = 0
@@ -74,10 +109,10 @@ class LLMPredictor():
             losses = {'control': [], 'pert': []}
             for (bcontrol, bpert, bpert_index) in (pbar := tqdm(iter(dl))):
                 curr_batch_size = bcontrol.shape[0]
-                for i in range(curr_batch_size // minibatch_size):
-                    control = bcontrol[(i * minibatch_size):((i + 1) * minibatch_size)]
-                    pert = bpert[(i * minibatch_size):((i + 1) * minibatch_size)]
-                    pert_index = bpert_index[(i * minibatch_size):((i + 1) * minibatch_size)]
+                for i in range(curr_batch_size // self.minibatch_size):
+                    control = bcontrol[(i * self.minibatch_size):((i + 1) * self.minibatch_size)]
+                    pert = bpert[(i * self.minibatch_size):((i + 1) * self.minibatch_size)]
+                    pert_index = bpert_index[(i * self.minibatch_size):((i + 1) * self.minibatch_size)]
                     pert_index = pert_index.squeeze()
                     active_weights = 10 * (control > 0).float().to(device) + 1 if use_active_weights else 1
                     control = control.float().to(self.device)
@@ -86,10 +121,10 @@ class LLMPredictor():
                     control_results = self.model(control, mask=use_mask_task)
                     control_recon = control_results[0]
                     
-                    control_loss = torch.sum(active_weights * torch.abs(control_recon - control)) / minibatch_size
+                    control_loss = torch.sum(active_weights * torch.abs(control_recon - control)) / self.minibatch_size
                     if self.config['use_sparsity_loss'] and len(control_results) == 3:
                         control_sparsity = control_results[1]
-                        control_loss += torch.sum(active_weights * torch.abs(control_sparsity - (control > 0).float())) / minibatch_size
+                        control_loss += torch.sum(active_weights * torch.abs(control_sparsity - (control > 0).float())) / self.minibatch_size
 
                     loss = control_loss
                     loss.backward()
@@ -112,52 +147,68 @@ class LLMPredictor():
         
         ## CODE FROM PERT RECONSTRUCTION LOOP ##
 
-        optim.zero_grad()
+        use_sparsity_loss = True
+        use_mask_task = False
+        use_active_weights = True
+        lr_step = 32
+
+        step_count = 0
+        self.optim.zero_grad()
         pert_task = 0
-        for e in range(2):
+        for e in range(20):
             model.train()
             losses = {'control': [], 'pert': []}
             for (bcontrol, bpert, bpert_index) in (pbar := tqdm(iter(dl))):
+                bcontrol, bpert, bpert_index = bcontrol.squeeze(), bpert.squeeze(), bpert_index.reshape(-1, 1)# , # bpert_expr.squeeze()
                 curr_batch_size = bcontrol.shape[0]
-                for i in range(curr_batch_size // minibatch_size):
-                    control = bcontrol[(i * minibatch_size):((i + 1) * minibatch_size)]
-                    pert = bpert[(i * minibatch_size):((i + 1) * minibatch_size)]
-                    pert_index = bpert_index[(i * minibatch_size):((i + 1) * minibatch_size)]
+                for i in range(curr_batch_size // self.minibatch_size):
+                    control = bcontrol[(i * self.minibatch_size):((i + 1) * self.minibatch_size)]
+                    if control.shape[0] == 0:
+                        continue
+                    pert = bpert[(i * self.minibatch_size):((i + 1) * self.minibatch_size)]
+                    pert_index = bpert_index[(i * self.minibatch_size):((i + 1) * self.minibatch_size)]
                     pert_index = pert_index.squeeze()
-                    active_weights = 10 * (control > 0).float().to(device) + 1 if use_active_weights else 1
-                    pert_active_weights = 10 * (pert > 0).float().to(device) + 1 if use_active_weights else 1
+                    # print(pert_index)
+                    active_weights = 100 * (control > 0).float().to(device) + 1 if use_active_weights else 1
+                    pert_active_weights = 100 * (pert > 0).float().to(device) + 1 if use_active_weights else 1
                     control = control.float().to(device)
                     pert = pert.float().to(device)
                     step_count += 1
 
                     pert_expr = pert[torch.arange(pert.size(0)), pert_index, None]
-                    control_results, pert_results = model(
+                    control_results, pert_results = self.model(
                         control, pert_expr=pert_expr, pert_index=pert_index, mask=use_mask_task, recon_and_pert=True
                     )
                     
                     control_recon, pert_recon = control_results[0], pert_results[0]
-                    control_loss = torch.sum(active_weights * torch.abs(control_recon - control)) / minibatch_size
-                    pert_loss = torch.sum(pert_active_weights * torch.abs(pert_recon - pert)) / minibatch_size
+                    control_loss = torch.sum(active_weights * torch.abs(control_recon - control)) / self.minibatch_size
+                    pert_loss = torch.sum(pert_active_weights * torch.abs(pert_recon - pert)) / self.minibatch_size
+                    
+                    mean_pert, mean_pert_recon = pert.mean(axis=0), pert_recon.mean(axis=0)
+                    mean_pert_loss = 10 * torch.sum(torch.abs(mean_pert - mean_pert_recon)) 
+                    
+                    std_pert, std_pert_recon = pert.std(axis=0), pert_recon.std(axis=0)
+                    std_pert_loss = 10 * torch.sum(torch.abs(std_pert - std_pert_recon)) 
                     
                     if use_sparsity_loss and len(control_results) == 3:
                         control_sparsity, pert_sparsity = control_results[1], pert_results[1]
-                        control_loss += torch.sum(active_weights * torch.abs(control_sparsity - (control > 0).float())) / minibatch_size
-                        pert_loss += torch.sum(pert_active_weights * torch.abs(pert_sparsity - (pert > 0).float())) / minibatch_size
+                        control_loss += torch.sum(active_weights * torch.abs(control_sparsity - (control > 0).float())) / self.minibatch_size
+                        pert_loss += torch.sum(pert_active_weights * torch.abs(pert_sparsity - (pert > 0).float())) / self.minibatch_size
 
-                    loss = (pert_loss + control_loss)
+                    loss = (pert_loss + control_loss + mean_pert_loss + std_pert_loss)
                     loss.backward()
-                    optim.step()
-                    optim.zero_grad()
+                    self.optim.step()
+                    self.optim.zero_grad()
                     losses['control'].append(control_loss.item())
                     losses['pert'].append(pert_loss.item())
                     if step_count % lr_step == 0:
-                        lr_scheduler.step()
+                        self.lr_scheduler.step()
                     pbar.set_description(
-                        f"tv: {np.array(losses['control'])[-lr_step:].mean():.3f}, ptv: {np.array(losses['pert'])[-lr_step:].mean():.3f}"
+                        f"loss: {loss:.3f}, tv: {np.array(losses['control'])[-lr_step:].mean():.3f}, ptv: {np.array(losses['pert'])[-lr_step:].mean():.3f}"
                     )
             
             avg_loss = sum(losses['control']) / len(losses['control'])
-            torch.save(model, f"{save_dir}{e}")
+            torch.save(self.model, f"{save_dir}{e}")
             # writer.add_scalar('mae_loss', avg_loss, global_step=e)
             print(f'In epoch {e}, average traning loss is {avg_loss}.')
 
@@ -167,4 +218,32 @@ class LLMPredictor():
 
     #I mean to we need to evaluate anything? 
     def make_predict(self, adata: sc.AnnData | torch.DataLoader, pert_id: str, cell_type: str) -> np.ndarray:
-        pass 
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        X = adata.obsm["standard"]
+        X = X.toarray()
+        X = X / X.sum(axis=1)[:, None]
+
+        cell_type_names = adata.obs[CELL_KEY]
+        pert_type_names = adata.obs[PERT_KEY]
+
+        control_eval =  torch.tensor(X[cell_type_names == cell_type]).float()# .to(device)
+        true_pert= torch.tensor(X[(pert_type_names == pert_id) & (cell_type_names == cell_type)]).float()# .to(device)
+        curr_batch_size = min(control_eval.shape[0], true_pert.shape[0])
+        pred_perts = []
+        for i in range(curr_batch_size // self.minibatch_size + 1):
+            print(i)
+            control = control_eval[(i * self.minibatch_size):min(curr_batch_size, ((i + 1) * self.minibatch_size))].to(device)
+            pert = true_pert[(i * self.minibatch_size):min(curr_batch_size, ((i + 1) * self.minibatch_size))].to(device)
+            pert_expr = pert[torch.arange(pert.shape[0]), pert_id, None]
+            pred_pert, _, = self.model(
+                control, pert_expr=pert_expr, pert_index=pert_id, mask=False
+            )
+            pred_pert = control.cpu()
+            control.cpu().numpy()
+            
+            pert.cpu().numpy()
+            pred_perts.append(pred_pert.cpu().detach().numpy())
+            torch.cuda.empty_cache()
+        pred_pert = np.vstack(pred_perts)
+        return pred_pert
