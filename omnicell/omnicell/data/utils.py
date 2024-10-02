1dce4617d4c7189c835460978d14751a0dc59e18
Oscar Pitcho <opitcho@ethz.ch>
2024-10-02 15:28:12 -0400

    Starting to shape up

--- omnicell/omnicell/data/utils.py
+++ omnicell/omnicell/data/utils.py
@@ -9,27 +9,6 @@ def prediction_filename(pert, cell):
     return f'pred_{pert}_{cell}'
 
 
-def split_anndata(adata: sc.AnnData, holdout_perts: Union[str, List[str]], holdout_cells: Union[str, List[str]], pert_key: str, cell_key: str) -> Tuple[sc.AnnData, sc.AnnData]:
-    """
-    Splits anndata object into two based on holdout perturbations and holdout cell types
-    
-    If a cell has either a holdout perturbation or a holdout cell type, it is put into the evaluation set
-    
-    returns a tuple of two anndata objects (train, eval) which are views of the original anndata object"""
-
-
-    holdout_perts = [holdout_perts] if isinstance(holdout_perts, str) else holdout_perts
-    holdout_cells = [holdout_cells] if isinstance(holdout_cells, str) else holdout_cells
-
-    idx_hold_perts = adata.obs[pert_key].isin(holdout_perts)
-    idx_hold_cells = adata.obs[cell_key].isin(holdout_cells)
-
-    idx_eval = idx_hold_perts | idx_hold_cells
-    idx_train = ~idx_eval
-
-    return adata[idx_train], adata[idx_eval]
-
-
 def get_pert_cell_data(adata: sc.AnnData, pert: str, cell: str) -> sc.AnnData:
     """
     Returns a view of the anndata object with only the specified perturbation on the specified cell type.
