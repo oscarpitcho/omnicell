56c58f0f804e33939cd6a5a6bd1f772a7efb74e7
Oscar Pitcho <opitcho@ethz.ch>
2024-10-04 16:32:19 -0400

    Fixing config

--- omnicell/omnicell/config/config.py
+++ omnicell/omnicell/config/config.py
@@ -1,5 +1,5 @@
 
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 #TODO: - Configs just for trainings, configs for evaluations, 
 
@@ -10,11 +10,10 @@ class Config:
     #So what is the config object, what does it do? - Some top level dictionary that contains all subjacent configs
     def __init__(self, config):
         #self.task_config = config['task'].copy() if 'task' in config else None
-        self.model_config = config['model'].copy() if 'model' in config else None
-        self.eval_config = config['eval'].copy() if 'eval' in config else None
-        self.train_args = config['train_args'].copy() if 'train_args' in config else None
-        self.timestamp = config['timestamp'] if 'timestamp' in config else None
+        self.model_config = config['model_config'].copy() if 'model_config' in config else None
+        self.eval_config = config['eval_config'].copy() if 'eval_config' in config else None
         self.data_config = config['data_config'].copy() if 'data_config' in config else None
+
 
     @classmethod
     def empty(cls):
@@ -34,24 +33,18 @@ class Config:
     #We completely control the serialization of the config object in this class 
     def to_dict(self):
         config = {}
-        if self.task_config is not None:
-            config['task'] = self.task_config.copy()
         if self.model_config is not None:
-            config['model'] = self.model_config.copy()
+            config['model_config'] = self.model_config.copy()
         if self.eval_config is not None:
-            config['eval'] = self.eval_config.copy()
-        if self.train_args is not None:
-            config['train_args'] = self.train_args.copy()
-        if self.timestamp is not None:
-            config['timestamp'] = self.timestamp
+            config['eval_config'] = self.eval_config.copy()
         if self.data_config is not None:
             config['data_config'] = self.data_config.copy()
         return config
     
     #Bunch of getters and setters to no longer be fixed by the config file structure in the code
-    def add_task_config(self, task_config)-> 'Config':
+    def add_data_config(self, data_config)-> 'Config':
         config = self.copy()
-        config.task_config = task_config
+        config.data_config = data_config
         return config
     
     def add_model_config(self, model_config) -> 'Config':
@@ -59,9 +52,9 @@ class Config:
         config.model_config = model_config
         return config
     
-    def add_data_config(self, data_config)-> 'Config':
+    def add_eval_config(self, eval_config)-> 'Config':
         config = self.copy()
-        config.data_config = data_config
+        config.eval_config = eval_config
         return config
     
     """
@@ -71,107 +64,69 @@ class Config:
         data_config = self.data_config.copy()
         model_config = self.model_config.copy()
         return Config({'data_config': data_config, 'model': model_config})
-
-    def add_eval_config(self, eval_config)-> 'Config':
-        config = self.copy()
-        config.eval_config = eval_config
-        return config
-
-    def add_train_args(self, train_args_dict)-> 'Config':
-        config = self.copy()
-        config.train_args = train_args_dict
-        return config
     
-    def get_training_dataset_name(self)-> str:
-        pass
     
-    def get_eval_config_name(self)-> str:
-        pass
-    
-    def get_data_loader_config(self)-> str:
-        return self.data_config['loader']
-    
-    def get_pert_key(self)-> str:
-        return self.task_config['data']['pert_key']
-
-    def get_cell_key(self)-> str:
-        return self.task_config['data']['cell_key']
-
-    def get_control_pert(self)-> str:
-        return self.task_config['data']['control']
-    
-
-    def get_cell_embedding_name(self)-> str:
-        pass
-
-    def get_var_names_key(self)-> str:
-        return self.task_config['data'].get('var_names_key', None)
-
-
-    def get_test_mode(self)-> bool:
-        return self.train_args['test_mode']
-
-    def set_target_evaluations(self, eval_targets: List[Tuple[str, str]])-> 'Config':
-        config = self.copy()
-
-        config.task_config['datasplit']['evals']['evaluation_targets'] = eval_targets
-        return config
-
-    def get_eval_targets(self)-> List[Tuple[str, str]]:
-        """Returns a list of [Cell, Pert] tuples that are used for evaluation"""
-        return self.task_config['datasplit']['evals']['evaluation_targets']
-    
-
-    def get_task_name(self)-> str:
-        return self.task_config['name']
-    
+    #GETTERS FOR MODEL
     def get_model_name(self)-> str:
         return self.model_config['name']
-
+    
     def get_model_config(self):
         return self.model_config.copy()
-    
-    def get_var_gene_key(self):
-        return self.task_config['data'].get(['gene_key'], None)
-    
 
+
+    #GETTERS FOR TRAINING
+    def get_training_dataset_name(self)-> str:
+        return self.data_config['data']['dataset']
+    
+    def get_data_config_name(self)-> str:
+        return self.data_config['name']
+    
+    """Returns mode of data split, iid or ood"""
     def get_mode(self):
-        return self.task_config['datasplit']['mode']
+        return self.data_config['datasplit']['mode']
     
 
     def get_heldout_cells(self):
         """Returns the heldout cells for the training data, returns an empty list if no cells are held out"""
-        return self.task_config['datasplit']['training'].get('holdout_cells', [])
+        return self.data_config['datasplit'].get('holdout_cells', [])
     
     def get_heldout_perts(self):
         """Returns the heldout perturbations for the training data, returns an empty list if no perturbations are held out"""
-        return self.task_config['datasplit']['training'].get('holdout_perts', [])
+        return self.data_config['datasplit'].get('holdout_perts', [])
     
 
     def get_apply_normalization(self) -> bool:
-        return self.task_config['data']['apply_norm']
+        return self.data_config['data']['count_norm']
     
-    def get_apply_log1p(self):
-        return self.task_config['data']['apply_log1p']
-    
-    def set_heldout_cells(self, heldout_cells: List[str]):
-        config = self.copy()
-        config.task_config['datasplit']['training']['holdout_cells'] = heldout_cells
-        return config
-    
-    def set_heldout_perts(self, heldout_perts: List[str]):
-        config = self.copy()
-        config.task_config['datasplit']['training']['holdout_perts'] = heldout_perts
-        return config
-    
-    def add_timestamp(self, timestamp: str):
-        config = self.copy()
-        config.timestamp = timestamp
-        return config
+    def get_apply_log1p(self) -> bool:
+        return self.data_config['data']['log1p']
+   
 
-    def get_data_path(self):
-        if self.task_config is None:
-            raise ValueError("No data config found in the config file")
-        return self.task_config['data']['path']
+    def get_cell_embedding_name(self) -> Optional[str]:
+        return self.data_config['data'].get('cell_embedding', None)
+    
+    def get_pert_embedding_name(self) -> Optional[str]:
+        return self.data_config['data'].get('pert_embedding', None) 
+    
 
+
+
+
+
+    #GETTERS FOR EVAL
+    def get_eval_config_name(self)-> str:
+        return self.eval_config['name']
+    
+    def get_eval_dataset_name(self)-> str:
+        return self.eval_config['dataset']
+    
+    def get_eval_targets(self)-> List[Tuple[str, str]]:
+        """Returns a list of [Cell, Pert] tuples that are used for evaluation"""
+        return self.eval_config['evaluation_targets']
+    
+
+    
+    
+
+    
     
