e471b066b53ebf5f1f207e4f2d91d0301797defb
Oscar Pitcho <opitcho@ethz.ch>
2024-10-14 10:48:22 -0400

    Fixing script for static embedding

--- omnicell/generate_static_embedding.py
+++ omnicell/generate_static_embedding.py
@@ -22,10 +22,18 @@ def main():
     parser.add_argument('--embedding_name', type=str, help='Name of the embedding, this will be used to save the embedding')
 
 
+
     #Can be None or Mean
-    parser.add_argument('--imputing_method', choices=['None', 'mean'], help='Imputing method to use, if None, perts with not data will be left out, if Mean, the mean of all pert embeddings will be used.')
+    parser.add_argument('--imputing_method', choices=['None', 'Mean'], help='Imputing method to use, if None, perts with not data will be left out, if Mean, the mean of all pert embeddings will be used.')
 
     args = parser.parse_args()
+
+    assert args.dataset_name is not None, "Please provide a dataset name"
+    assert args.static_embedding_path is not None, "Please provide a path to the static embedding"
+    assert args.embedding_name is not None, "Please provide a name for the embedding"
+    assert args.imputing_method is not None, "Please provide an imputing method"
+
+
 
     catalogue = json.load(open(DATA_CATALOGUE_PATH))
 
@@ -53,7 +61,11 @@ def main():
 
     static_embedding = torch.load(args.static_embedding_path)
 
-    mean_embedding = torch.mean(torch.cat(list(static_embedding.values())), axis = 0)
+    all_embeddings = torch.stack(list(static_embedding.values()))
+
+    print(f"Shape of static embedding: {all_embeddings.shape}")
+
+    mean_embedding = torch.mean(all_embeddings, axis = 0)
 
     embedding = {}
 
@@ -71,10 +83,16 @@ def main():
             elif args.imputing_method == 'Mean':
                 embedding[pert] = mean_embedding
 
+            else:
+                raise ValueError(f"Unrecognized imputing method: {args.imputing_method}")
 
+
+
+    #Overwrites any existing file with the same name
     torch.save(embedding, f"{ds_details.folder_path}/{args.embedding_name}.pt")
 
     print(f'Total coverage: {coverage}/{len(perts)}')
+    print(f"Size of the embedding: {len(embedding)}")
 
 
     #TODO: Automatically extend json catalogue with the new embedding
