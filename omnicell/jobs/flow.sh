4b659d1d602fedb9d8b99082686c41a1b53832cf
njwfish <njwfish@gmail.com>
2024-10-14 11:06:56 -0400

    flow test config

--- omnicell/jobs/flow.sh
+++ omnicell/jobs/flow.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 48:00:00          # walltime = 48 hours
-#SBATCH -n 4                 # 4 CPU cores
+#SBATCH --ntasks-per-node=4  # 4 CPU cores
 #SBATCH --gres=gpu:1 --constraint=high-capacity  # 1 non-A100 GPU 
 #SBATCH --mem=128G           # memory per node
 hostname                     # Print the hostname of the compute node
