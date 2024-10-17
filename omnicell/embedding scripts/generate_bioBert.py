11709bd1dbfe3a8920844ea0da737d6d8d77cf10
Oscar Pitcho <opitcho@ethz.ch>
2024-10-17 16:16:13 -0400

    Updating catalogue with new embedding locations

--- omnicell/embedding scripts/generate_bioBert.py
+++ omnicell/embedding scripts/generate_bioBert.py
@@ -19,17 +19,11 @@ def main():
     parser = argparse.ArgumentParser(description='Generate static embedding')
 
     parser.add_argument('--dataset_name', type=str, help='Name of the dataset')
-    parser.add_argument('--embedding_name', type=str, help='Name of the embedding, this will be used to save the embedding')
-
-    #Can be None or Mean
-    parser.add_argument('--imputing_method', choices=['None', 'Mean'], help='Imputing method to use, if None, perts with not data will be left out, if Mean, the mean of all pert embeddings will be used.')
 
     args = parser.parse_args()
 
     assert args.dataset_name is not None, "Please provide a dataset name"
-    assert args.static_embedding_path is not None, "Please provide a path to the static embedding"
-    assert args.embedding_name is not None, "Please provide a name for the embedding"
-    assert args.imputing_method is not None, "Please provide an imputing method"
+
 
 
 
@@ -38,61 +32,45 @@ def main():
     #Getting the dataset details from the data_catalogue.json
 
     ds_details = catalogue.get_dataset_details(args.dataset_name)
+    pert_key = ds_details.pert_key
+    control_pert = ds_details.control_pert
+    
+    adata = sc.read(ds_details.path, backed='r')
 
-        # Load tokenizer
+
+    perts = [x for x in adata.obs[pert_key].unique() if x != control_pert]
+
+
+    # Load tokenizer
     tokenizer = AutoTokenizer.from_pretrained("dmis-lab/biobert-v1.1")
 
     # Load model
     model = AutoModel.from_pretrained("dmis-lab/biobert-v1.1")
 
-    pert_key = ds_details.pert_key
-    control_pert = ds_details.control
 
-    #Loading the dataset
-    adata = sc.read(ds_details.path, backed='r')
+    embeddings = {}
 
-    #Selecting all non Control Perts
-    perts = [x for x in adata.obs[pert_key].unique() if x != control_pert]
-
-    #Getting the static embedding
-
-    static_embedding = torch.load(args.static_embedding_path)
-
-    all_embeddings = torch.stack(list(static_embedding.values()))
-
-    print(f"Shape of static embedding: {all_embeddings.shape}")
-
-    mean_embedding = torch.mean(all_embeddings, axis = 0)
-
-    embedding = {}
-
-    coverage = 0
     for pert in perts:
+        text = pert.upper()
 
-        if pert in static_embedding:
-            embedding[pert] = static_embedding[pert]
-            coverage += 1
-        else:
-            if args.imputing_method == 'None':
-                embedding[pert] = None
+        inputs = tokenizer(text, return_tensors="pt")
 
-            elif args.imputing_method == 'Mean':
-                embedding[pert] = mean_embedding
+        # Get the embeddings
+        outputs = model(**inputs)
 
-            else:
-                raise ValueError(f"Unrecognized imputing method: {args.imputing_method}")
+        embeddings = torch.squeeze(outputs.pooler_output)
 
+        embeddings[pert] = embeddings
 
 
     #Overwrites any existing file with the same name
-    torch.save(embedding, f"{ds_details.folder_path}/{args.embedding_name}.pt")
+    torch.save(embeddings, f"{ds_details.folder_path}/{args.embedding_name}.pt")
 
-    print(f'Total coverage: {coverage}/{len(perts)}')
-    print(f"Size of the embedding: {len(embedding)}")
+    print(f"Size of the embedding: {len(embeddings)}")
 
 
     #Register the new embedding in the catalogue, This modifies the underlying yaml file
-    catalogue.register_new_pert_embedding(args.dataset_name, args.embedding_name)
+    catalogue.register_new_pert_embedding(args.dataset_name, "BioBERT")
 
 
 
