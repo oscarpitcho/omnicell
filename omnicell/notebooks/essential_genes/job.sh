93c53b57bdec6eae72bce09ccbc2c5f04c301967
Oscar Pitcho <opitcho@ethz.ch>
2024-10-25 17:33:15 -0400

    Generating #of DEGs for essential_genes

---
+++ omnicell/notebooks/essential_genes/job.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -t 12:00:00          # walltime = 2 hours
+#SBATCH --ntasks-per-node=4  # 4 CPU cores
+#SBATCH --mem=128GB          # memory per node
+hostname
+
+source ~/.bashrc
+conda activate sandbox
+
+
+python -m notebooks.essential_genes.generate_DEGs_per_pert 
+
+echo "Finished successfully"
