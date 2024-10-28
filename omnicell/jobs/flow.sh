8a63771ccb53e72b8059fa53de09704f129afa21
njwfish <njwfish@gmail.com>
2024-10-28 07:46:07 -0400

    flow updatse

--- omnicell/jobs/flow.sh
+++ omnicell/jobs/flow.sh
@@ -16,6 +16,10 @@ conda activate sandbox
 python train.py --data_config configs/splits/satija_raw/satija_across_genes_hvg.yaml --model_config configs/models/flow.yaml \
  --eval_config configs/evals/satija_raw/ev_satija_across_genes_hvg.yaml -l DEBUG
 
+python train.py --etl_config configs/satija_INFB_raw/etl/preprocess_and_UCE_embedding.yaml \
+ --datasplit_config configs/satija_INFB_raw/splits/ho_IFNAR2.yaml \
+ --eval_config configs/satija_INFB_raw/evals/ev_IFNAR2_A549.yaml \
+ --model_config configs/models/flow.yaml -l DEBUG
 
 # python generate_evaluations.py --root_dir ./results/nn/satija_across_genes
 
