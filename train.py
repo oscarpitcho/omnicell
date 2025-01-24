11375317f5971f009c6d607ca13ba92eca6f9883
opitcho <opitcho@node339.inband>
2025-01-24 12:34:24 -0500

    Done with control predictor

--- train.py
+++ train.py
@@ -92,6 +92,11 @@ def get_model(model_name, config_model, loader, pert_rep_map, input_dim, device,
         from omnicell.models.mean_models.model import MeanPredictor
         logger.info("Mean model selected")
         model = MeanPredictor(config_model, pert_rep_map)
+    elif "control_predictor" in model_name:
+        from omnicell.models.dummy_predictors.control_predictor import ControlPredictor
+        logger.info("Control model selected")
+        adata_cheat = loader.get_complete_training_dataset()
+        model = ControlPredictor(adata_cheat)
         
     else:
         raise ValueError(f'Unknown model name {model_name}')
