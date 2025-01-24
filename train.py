bc468fbf0111b9231b2a49c82461004148278d06
opitcho <opitcho@node339.inband>
2025-01-24 17:52:54 -0500

    Fixing mean shift

--- train.py
+++ train.py
@@ -97,6 +97,12 @@ def get_model(model_name, config_model, loader, pert_rep_map, input_dim, device,
         logger.info("Control model selected")
         adata_cheat = loader.get_complete_training_dataset()
         model = ControlPredictor(adata_cheat)
+    
+    elif "mean_shift_dist" in model_name:
+        from omnicell.models.dummy_predictors.mean_shift_dist import MeanShiftDistributionPredictor
+        logger.info("Mean Shift Distribution model selected")
+        adata_cheat = loader.get_complete_training_dataset()
+        model = MeanShiftDistributionPredictor(adata_cheat)
         
     else:
         raise ValueError(f'Unknown model name {model_name}')
