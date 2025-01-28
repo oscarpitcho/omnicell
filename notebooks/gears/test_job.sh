da4e32ed1526190a6fa668ca8d10d100a0e11e6e
opitcho <opitcho@node1702.inband>
2025-01-28 18:36:55 -0500

    Bunch of changes + doc on the changes

--- notebooks/gears/test_job.sh
+++ notebooks/gears/test_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -t 12:00:00
-#SBATCH -p ou_bcs_low
+#SBATCH -p ou_bcs_normal
 #SBATCH --gres=gpu:h100:1
 #SBATCH --mem=200GB
 echo hostname
@@ -9,6 +9,6 @@ source ~/.bashrc
 conda activate omnicell
 
 
-python -m notebooks.gears.test_preproc_script
+python -m notebooks.gears.test_script
 
 echo "Job finished"
--- notebooks/gears/test_script.py
+++ notebooks/gears/test_script.py
@@ -2,7 +2,11 @@ import sys
 
 from omnicell.models.gears.pertdata import PertData
 from omnicell.data.catalogue import Catalogue
+from omnicell.models.gears.gears import GEARS
 import scanpy as sc
+import torch
+import numpy as np
+
 dd = Catalogue.get_dataset_details('repogle_k562_essential_raw')
 adata = sc.read(dd.path)
 
@@ -11,18 +15,46 @@ perts = [p for p in adata.obs["condition"].unique() if p != dd.control]
 adata.obs["condition"] = adata.obs["condition"].replace({dd.control:"ctrl"})
 adata.obs["condition"] = adata.obs["condition"].replace({p:p+"+ctrl" for p in perts})
 
+
+print(f"Data loaded from {dd.path}")
 sc.pp.normalize_total(adata)
 sc.pp.log1p(adata)
 sc.pp.highly_variable_genes(adata,n_top_genes=5000, subset=True)
+print(f"Data normalized and log transformed")
 
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 pert_data = PertData('./data') # specific saved folder
-pert_data.new_data_process(dataset_name = 'repogle', adata = adata) # specific dataset name and adata object
+print(f"Saving data in ./data")
+
+pert_data.new_data_process(dataset_name = 'repogle', adata = adata, skip_calc_de=True) # specific dataset name and adata object
 print(f"Data processed and saved in {pert_data.data_path}")
 pert_data.load(data_path = './data/repogle') # load the processed data, the path is saved folder + dataset_name
 print(f"Data loaded from {pert_data.data_path}")
-pert_data.prepare_split(split = 'simulation', seed = 1) # get data split with seed
+pert_data.prepare_split(split = 'no_test', seed = 1) # get data split with seed
 print(f"Data split with seed 1")
 pert_data.get_dataloader(batch_size = 32, test_batch_size = 128) # prepare data loader
 print(f"Data loader prepared")
+
+
+gears_model = GEARS(pert_data, device = device, 
+                        weight_bias_track = False, 
+                        proj_name = 'pertnet', 
+                        exp_name = 'pertnet')
+
+gears_model.model_initialize(hidden_size = 64)
+
+gears_model.tunable_parameters()
+
+gears_model.train(epochs = 1, lr = 1e-3)
+
+
+gears_model.save_model('test_model')
+gears_model.load_pretrained('test_model')
+
+
+res = gears_model.predict([['RPL15']])
+
+np.savez('res.npz', res)
+
