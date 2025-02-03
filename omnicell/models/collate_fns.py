1846567f3924fab23cbcbffcff36b8fb170b6f5e
njwfish <njwfish@node1703.inband>
2025-02-02 20:19:23 -0500

    created basic dataloader

--- omnicell/models/collate_fns.py
+++ omnicell/models/collate_fns.py
@@ -6,40 +6,6 @@ from torchcfm.conditional_flow_matching import (
 import numpy as np
 
 
-def fm_collate(
-    batch,
-    return_noise=False,
-    return_control=False,
-    FM=ExactOptimalTransportConditionalFlowMatcher(sigma=0.1),
-):
-    noises = []
-
-    control, pert = zip(*batch)
-    control = torch.tensor(np.array(control))
-    pert = torch.tensor(np.array(pert))
-
-    if return_noise:
-        t, xt, ut, eps = FM.sample_location_and_conditional_flow(
-            control,
-            pert,
-            return_noise=return_noise,
-        )
-        noises.append(eps)
-    else:
-        t, xt, ut = FM.sample_location_and_conditional_flow(
-            control,
-            pert,
-            return_noise=return_noise,
-        )
-    ret = [t, xt, ut]
-    if return_noise:
-        noises = torch.cat(noises)
-        ret.append(noises)
-    if return_control:
-        ret.append(control)
-    return ret
-
-
 def cfm_collate(
     batch,
     return_noise=False,
@@ -77,7 +43,6 @@ def cfm_collate(
 
 def ot_collate(
     batch,
-    return_noise=False,
     ot_sampler = OTPlanSampler(method="exact")
 ):
 
@@ -88,3 +53,12 @@ def ot_collate(
     x0, x1 = ot_sampler.sample_plan(control, target)
 
     return x0, x1, *batch[2:]
+
+def collate(
+    batch,
+):
+
+    batch = list(zip(*batch))
+    batch = [torch.tensor(np.array(x)) for x in batch]
+
+    return batch
--- omnicell/models/datamodules.py
+++ omnicell/models/datamodules.py
@@ -12,11 +12,8 @@ from collections import defaultdict
 
 class StratifiedBatchSampler(Sampler[List[int]]):
     def __init__(
-        self, ns, batch_size: int
+        self, ns, batch_size: int, samples_per_epoch: int = None
     ) -> None:
-        # Since collections.abc.Iterable does not check for `__getitem__`, which
-        # is one way for an object to be an iterable, we don't do an `isinstance`
-        # check here.
         if not isinstance(batch_size, int) or isinstance(batch_size, bool) or \
                 batch_size <= 0:
             raise ValueError(f"batch_size should be a positive integer value, but got batch_size={batch_size}")
@@ -27,27 +24,19 @@ class StratifiedBatchSampler(Sampler[List[int]]):
         print("Strata probs", np.sort(self.probs))
         self.batch_size = batch_size
         self.batch_sizes = np.minimum(ns, batch_size)
+        if samples_per_epoch is not None:
+            self.samples_per_epoch = samples_per_epoch
+        else:
+            self.samples_per_epoch = np.sum(self.ns)
     
     def get_random_stratum(self):
         linear_idx = np.random.choice(self.num_strata, p=self.probs)
         stratum = np.unravel_index(linear_idx, self.ns.shape)
         return stratum
 
-    # def __iter__(self) -> Iterator[List[int]]:
-    #     # Implemented based on the benchmarking in https://github.com/pytorch/pytorch/pull/76951
-    #    while True:
-    #         stratum = self.get_random_stratum()
-    #         try:
-    #             batch_stratum = np.repeat(np.array(stratum)[None, :], self.batch_sizes[stratum], axis=0)
-    #             batch = np.random.choice(self.ns[stratum], self.batch_sizes[stratum], replace=False)
-    #             yield zip(batch_stratum, batch)
-    #         except StopIteration:
-    #             break
-
-
     def __iter__(self) -> Iterator[List[int]]:
         # Calculate number of batches based on total samples and batch size
-        samples_remaining = np.sum(self.ns)
+        samples_remaining = self.samples_per_epoch
         while True:
             if samples_remaining < 0:
                 break
@@ -58,18 +47,14 @@ class StratifiedBatchSampler(Sampler[List[int]]):
             yield zip(batch_stratum, batch)
 
     def __len__(self) -> int:
-        # Can only be called if self.sampler has __len__ implemented
-        # We cannot enforce this condition, so we turn off typechecking for the
-        # implementation below.
-        # Somewhat related: see NOTE [ Lack of Default `__len__` in Python Abstract Base Classes ]
         return np.sum(self.ns) // self.batch_size
 
-class SCFMDataset(torch.utils.data.Dataset):
+class StratifiedDataset(torch.utils.data.Dataset):
     def __init__(
         self, source, target, pert_ids, pert_map, source_strata, target_strata
     ):
         source, target = np.array(source), np.array(target)
-        pert_ids = np.array(pert_ids) # , np.array(pert_mat)
+        pert_ids = np.array(pert_ids)
         
         assert target.shape[0] == pert_ids.shape[0]
         assert source.shape[0] == source_strata.shape[0]
@@ -80,7 +65,6 @@ class SCFMDataset(torch.utils.data.Dataset):
         self.target_strata = target_strata
         self.strata = np.unique(source_strata)
         self.num_strata = len(self.strata)
-        print(source_strata, self.strata, self.num_strata)
         
         self.unique_pert_ids = np.unique(pert_ids)
         
@@ -118,32 +102,14 @@ class SCFMDataset(torch.utils.data.Dataset):
         (stratum_idx, pert_idx), idx = strata_idx
         stratum, pert = self.strata[stratum_idx], self.unique_pert_ids[pert_idx]
         sidx = np.random.choice(self.source[stratum].shape[0])
-        print()
         return (
             self.source[stratum][sidx],
             self.target[stratum][pert][idx],
             self.pert_map[self.pert_ids[stratum][pert][idx]],
         )
 
-def get_identity_features(adata, cell_type_features=True):
-    perts = pd.get_dummies(adata.obs[PERT_KEY]).values.astype(float)
-    cell_types = pd.get_dummies(adata.obs[CELL_KEY]).values
-    if cell_type_features:
-        combo = pd.get_dummies(adata.obs[CELL_KEY].astype(str) + adata.obs[PERT_KEY].astype(str)).values
-        idx = (combo!=0).argmax(axis=0)
-        pert_mat = np.hstack([cell_types, perts])[idx, :].astype('float32')
-    else:
-        combo = perts
-        idx = (combo!=0).argmax(axis=0)
-        pert_mat = perts[idx, :].astype('float32')
-    
-    pert_ids = combo.argmax(axis=1)
-    cell_types = cell_types.argmax(axis=1)
-    return pert_ids, pert_mat, cell_types
-
-
 def get_dataloader(
-        adata, batch_size=512, verbose=0, pert_map=None, pert_ids=None, collate='ot', X=None
+        adata, pert_map, pert_ids, batch_size=512, verbose=0, collate='ot', X=None
 ):
         if X is None:
             X = adata.X.toarray()
@@ -153,8 +119,6 @@ def get_dataloader(
         pert_idx = adata.obs[PERT_KEY] != CONTROL_PERT
         cell_types = adata.obs[CELL_KEY].values
 
-        if pert_map is None:
-            pert_ids, pert_map, cell_types = get_identity_features(adata)
 
         control_train = X[control_idx]
         pert_train = X[pert_idx]
@@ -172,7 +136,7 @@ def get_dataloader(
         else:
             raise ValueError(f"Collate function {collate} not recognized")
 
-        dset = SCFMDataset(
+        dset = StratifiedDataset(
             control_train, pert_train, 
             pert_ids_train, pert_map, 
             control_cell_types, pert_cell_types
