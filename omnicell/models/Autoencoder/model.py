14915b9d24c494c01dd5e0e0c672959657719889
jason-lequyer <163599822+jason-lequyer@users.noreply.github.com>
2025-02-06 12:17:56 -0500

    Update model.py

--- omnicell/models/Autoencoder/model.py
+++ omnicell/models/Autoencoder/model.py
@@ -271,7 +271,7 @@ class autoencoder(nn.Module):
         
         optimizer = optim.Adam(self.parameters(), lr=1e-3)
         start_epoch = 0
-        num_epochs = 2
+        num_epochs = 3
         
         print_interval = 5000
         
