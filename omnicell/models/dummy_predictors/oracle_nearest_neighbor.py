89dfb0c916c886fede3b45c5a9ca0a0df7b389a3
oscarpitcho <oscar.pitcho@live.com>
2025-02-23 14:44:07 -0500

    Fixing some bugs

--- omnicell/models/dummy_predictors/oracle_nearest_neighbor.py
+++ omnicell/models/dummy_predictors/oracle_nearest_neighbor.py
@@ -12,22 +12,13 @@ logger = logging.getLogger(__name__)
 
 class OracleNNPredictor():
 
-    def __init__(self, adata: sc.AnnData, model_config: dict):
-
-        assert "DEGs" in adata.uns.keys(), "DEGs not found in adata.uns"
+    def __init__(self, model_config: dict):
 
         self.model = None
-        self.total_adata = adata
+        self.model_config = model_config
 
-        self.DEGs = adata.uns["DEGs"].copy()
+        self.DEGs = None 
         self.number_top_DEGs_overlap = model_config["number_top_DEGs_overlap"]
-
-        for cell in self.DEGs:
-            for pert in self.DEGs[cell]:
-                df = pd.DataFrame.from_dict(self.DEGs[cell][pert], orient='index')
-                df = df[df['pvals_adj'] < model_config['p_threshold']]
-                self.DEGs[cell][pert] = df
-
         self.seen_pert_per_cell = None
         self.training_adata = None
 
@@ -39,7 +30,17 @@ class OracleNNPredictor():
         for cell in adata.obs[CELL_KEY].unique():
             self.seen_pert_per_cell[cell] = [p for p in adata.obs[PERT_KEY].unique() if p != adata.uns[CONTROL_PERT]]
 
+
+
         self.training_adata = adata
+        self.DEGs = adata.uns['DEGs']
+
+        for cell in self.DEGs:
+            for pert in self.DEGs[cell]:
+                df = pd.DataFrame.from_dict(self.DEGs[cell][pert], orient='index')
+                df = df[df['pvals_adj'] <self.model_config['p_threshold']]
+                self.DEGs[cell][pert] = df
+
 
 
     def make_predict(self, adata: sc.AnnData, pert_id: str, cell_type: str) -> np.ndarray:
@@ -70,24 +71,27 @@ class OracleNNPredictor():
 
         #We are making a prediction within a cell type, across perts, finding seen pert with highest overlap with target pert
         else:
-            logger.debug("Running NN Oracle across perts")
-            DEGs_cell_type = self.DEGs[cell_type]
+            # Looking for the seen perturbation with the highest overlap with the target pert
 
-            non_target_perts_in_cell = [p for p in DEGs_cell_type.keys() if p != pert_id]
+            logger.debug("Running NN Oracle across perts")
+
+            DEGs_per_pert = self.DEGs[cell_type]
+            DEGs_target = self.DEGs[cell_type][pert_id]
+
+            non_target_perts_in_cell = [p for p in DEGs_per_pert.keys() if p != pert_id]
 
             DEG_overlaps = {}
-            DEG_target = DEGs_cell_type[pert_id]
 
             for p in non_target_perts_in_cell:
                 if p in self.seen_pert_per_cell[cell_type]:
-                    DEG_p = DEGs_cell_type[p]
-                    DEG_overlaps[p] = len(DEG_target.index[:self.number_top_DEGs_overlap].intersection(DEG_p.index[:self.number_top_DEGs_overlap]))
+                    DEGs_p = DEGs_per_pert[p]
+                    DEG_overlaps[p] = len(DEGs_target.index[:self.number_top_DEGs_overlap].intersection(DEGs_p.index[:self.number_top_DEGs_overlap]))
 
             closest_pert = max(DEG_overlaps, key=DEG_overlaps.get)
 
         
             logger.info(f"Selected perturbation {closest_pert} as closest to {pert_id} in cell type {cell_type}, with {DEG_overlaps[closest_pert]} overlapping DEGs")
 
-            nn_population = self.training_adata[(self.total_adata.obs[PERT_KEY] == closest_pert) & (self.total_adata.obs[CELL_KEY] == cell_type)]
+            nn_population = self.training_adata[(self.training_adata.obs[PERT_KEY] == closest_pert) & (self.training_adata.obs[CELL_KEY] == cell_type)]
             return nn_population.X
     
